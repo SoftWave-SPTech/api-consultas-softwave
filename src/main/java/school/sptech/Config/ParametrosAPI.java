cc8a3664c66df4e82b5f135b1ab427b31960d213
Lua ♡ <160372370+lualuaSantosS2@users.noreply.github.com>
2025-12-08 12:11:31 -0300

    Update ParametrosAPI.java

--- src/main/java/school/sptech/Config/ParametrosAPI.java
+++ src/main/java/school/sptech/Config/ParametrosAPI.java
@@ -1,7 +1,7 @@
 package school.sptech.Config;
 
 public class ParametrosAPI {
-    private static String TOKEN = "uQv705cREM3_PsHNXAN7xDMgRYMUnveVSO1sSGrA";
+    private static String TOKEN = "OMdLdpCD-RiCiiEu6UqVsI8wPc75d6kS1wJZ5lpL";
     private static String TIMEOUT = "600";
     private static String PARAMETRO_PROCESSO = "";
     private static String PARAMETRO_PARTE = "";
